e23bdfa247cb45f0bef268da3279f97f98faf686
HyeongjinKim <79950123+HyeongjinKim98@users.noreply.github.com>
2023-08-21 14:36:29 +0900

    [Gold III] Title: 줄 세우기, Time: 1020 ms, Memory: 60476 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	static int N,M;
+	static List<Integer>[] col;
+	static boolean[] v;
+	
+	static void solve(int i) {
+		v[i] = true;
+		for (int j = 0; j < col[i].size(); j++) {
+			int next= col[i].get(j);
+			if(!v[next]) {
+				v[next] = true;
+				solve(next);
+			}
+		}
+		System.out.print(i + " ");
+	}
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st= new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		col = new ArrayList[N+1];
+		v = new boolean[N+1];
+		for (int i = 1; i < col.length; i++) {
+			col[i] = new ArrayList<Integer>();
+		}
+		for (int i = 0; i < M; i++) {
+			st= new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			col[b].add(a);
+		}
+		
+		for (int i = 1; i <=N; i++) {
+			if(!v[i]) {
+				solve(i);
+			}
+		}
+	}
+}
