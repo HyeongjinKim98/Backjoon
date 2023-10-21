40ba92ad1f911be75019da1750ab38cc691282af
HyeongjinKim <79950123+HyeongjinKim98@users.noreply.github.com>
2023-10-22 01:30:38 +0900

    [level 2] Title: 모음 사전, Time: 2.96 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,23 +1,18 @@
-class Solution{
-    static int answer = 0;
-    static boolean complete = false;
+class Solution {
     static String[] vowels = {"A","E","I","O","U"};
-    static void comb(String target, String word, int depth){
-        if(depth>5){
-            return;
-        }
-        if(target.equals(word)){
-            complete = true;
-            return;
-        }
-        for (int i = 0; i < 5; i++) {
-            if(complete) return;
-            if(depth < 5) answer ++;
-            comb(target + vowels[i], word,depth +1);
-        }
+    
+    public int solution(String word) {
+        
+        return comb("", word ,0);
     }
-    public int solution(String word){
-        comb("", word,0);
-        return answer;
+    
+    static int comb(String target, String word, int cnt){
+        if(word.equals(target)){
+            return cnt;
+        }
+        for(int i = 0; i <5; i++){
+            comb(target+vowels[i],word,cnt+1);
+        }
+        return 0;
     }
 }
