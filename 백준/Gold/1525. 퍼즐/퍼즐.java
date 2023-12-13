26ca8455784744da337a136c8f72c45a3b11ef61
HyeongjinKim <79950123+HyeongjinKim98@users.noreply.github.com>
2023-12-14 00:57:20 +0900

    [Gold II] Title: 퍼즐, Time: 900 ms, Memory: 117260 KB -BaekjoonHub

--- 백준/Gold/1525. 퍼즐/퍼즐.java
+++ 백준/Gold/1525. 퍼즐/퍼즐.java
@@ -8,7 +8,7 @@ public class Main {
     static int[] di = {-1, 0, 1, 0};
     static int[] dj = { 0, 1, 0,-1};
     static final String answer = "123456780";
-    static void bfs(String init){
+    static int bfs(String init){
         Queue<String> q = new ArrayDeque<>();
         q.offer(init);
 
@@ -19,6 +19,7 @@ public class Main {
             int i = zero%3;
             int j = zero/3;
 
+            if(map.equals(answer)) return depth;
             for(int d = 0; d <4 ; d++){
                 int ni = i + di[d];
                 int nj = j + dj[d];
@@ -39,6 +40,7 @@ public class Main {
                 }
             }
         }
+        return -1;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -53,12 +55,6 @@ public class Main {
             }
         }
         check.put(map, 0);
-        bfs(map);
-
-        if(check.get(answer) == null){
-            System.out.println(-1);
-        }else{
-            System.out.println(check.get(answer));
-        }
+        System.out.println(bfs(map));
     }
 }
